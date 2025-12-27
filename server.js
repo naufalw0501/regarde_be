a647ce5a717dadadb190380f151fa55bdfb877eb
unknown <naufal.wijaya9a@gmail.com>
2025-12-27 17:16:23 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+// fallback untuk React Router
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`React build running at http://localhost:${PORT}`);
+});
